340eddd4655c1054cb6e589f98decf7ed0aaa40d
Ikramul-m <i20727172@gmail.com>
2024-02-21 17:18:15 +0530

    Hi everyone

---
+++ src/app/page.tsx
@@ -0,0 +1,57 @@
+import Link from "next/link";
+
+export default function Home() {
+  return <>
+
+  <h1>Hello Beta</h1>
+
+    {/* <nav>
+
+      <label htmlFor="">Colorib</label>
+
+      <ul className="menu-bar">
+
+        <li>
+          <Link href="/">
+            HOME
+          </Link>
+        </li>
+
+        <li>
+          <Link href="about">
+            ABOUT
+          </Link>
+        </li>
+
+
+        <li>
+          <Link href="domain">
+            DOMAIN
+          </Link>
+        </li>
+
+
+        <li>
+          <Link href="hosting">
+            HOSTING
+          </Link>
+        </li>
+
+
+        <li>
+          <Link href="blog">
+            BLOG
+          </Link>
+        </li>
+
+
+        <li>
+          <Link href="contact">
+            CONTACT
+          </Link>
+        </li>
+
+      </ul>
+    </nav> */}
+  </>;
+}
